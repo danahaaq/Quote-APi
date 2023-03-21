e8990b01e3f17b9a374931eb0892e098508820d6
Aldanah Alqabbani <danaalialq@gmail.com>
2023-03-21 16:31:06 +0300

    lasts steps

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,13 +4,11 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     app.databases.use(.postgres(hostname: "localhost", username: "postgres", password: "" , database: "qt"), as: .psql)
-    // table name createSongs()
+    
     app.migrations.add(CreateAuthor())
     app.migrations.add(createQuoteeee2())
-    // wait means : wait until line 19 run or loading
     try app.autoMigrate().wait()
     // register routes
     try routes(app)
